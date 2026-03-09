b07ea5f3fcdd5b59898a6db80d349e85bd67b687
Wish-Upon-A-Star <57494067+Wish-Upon-A-Star@users.noreply.github.com>
2026-03-09 22:53:06 +0900

    Update 1.py

--- week2/1.py
+++ week2/1.py
@@ -1,12 +1,14 @@
-# 3000개의 숫자
-# -100~100 (201개)
-
-
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        dic={}
-        for i in nums:
-            dic[i]=True
-        return dict
+        numss=[]
+        isnum=[0 for i in range(202)]
 
-        
+        for i in nums:
+            if isnum[i+100]==1:continue
+            else:
+                 isnum[i+100]=1
+                 numss.append(i)
+
+        for i in range(len(numss)):
+            nums[i]=numss[i]
+        return len(numss)
